5b8725be7516bec28727e130d877878713a818ce
sarveswaran <sarvesh.farshore@gmail.com>
2018-10-08 22:46:44 +0530

    * debugging tag API

--- Modules/Content/Http/Controllers/Api/SearchController.php
+++ Modules/Content/Http/Controllers/Api/SearchController.php
@@ -52,7 +52,7 @@ class SearchController extends BasePublicController
 	public function categoryAndTaglist(Request $request, Client $http)
 	{
 		return [
-			'test' => DB::table('category')->select('id', 'name', 'slug_name')->where('status', '=', 1)->get(),
+			'test' => DB::table('content__categories')->select('id', 'name', 'slug_name')->where('status', '=', 1)->get(),
 			'category' => collect($this->category->getByAttributes(['status' => 1]))->map(function ($cat) {
 				return [
 					'id'        => $cat['id'],
