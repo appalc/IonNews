09a3b9356602132194011abce8ce4d376dff96ed
sarveswaran <sarvesh.farshore@gmail.com>
2019-01-24 12:11:08 +0530

    * fixed in homepage aPI

--- Modules/Content/Http/Controllers/Api/StoryController.php
+++ Modules/Content/Http/Controllers/Api/StoryController.php
@@ -165,9 +165,9 @@ class StoryController extends BasePublicController
 		}
 
 		// To sort it by decending order of created date
-		$dataresponse = collect($dataresponse)->->filter(function ($stories)
+		$dataresponse = collect($dataresponse)->->filter(function ($stories) {
 			return !empty($stories['stories']);
-		)->mapWithKeys(function ($stories, $catName) {
+		})->mapWithKeys(function ($stories, $catName) {
 			return [
 				$catName => [
 					'stories' => collect($stories['stories'])->sortByDesc('created_at')->values(),
